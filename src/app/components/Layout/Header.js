f5852fe7d61e41577f5d70a5bb930c84554298ab
cs-mmeza <mmeza1701@gmail.com>
2024-02-19 17:19:04 -0500

    corrections

--- src/app/components/Layout/Header.js
+++ src/app/components/Layout/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./header.module.css";   
+import styles from "./Header.module.css";   
 import { useDarkMode, toggleDarkMode } from "@/app/components/context/DarkModeContext";
 
 
